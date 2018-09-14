b7b9aa626698059d6edc21fa02ae06fdc17e2a19
SSolanoB <s.solano586@uniandes.co>
2018-09-14 10:52:36 -0500

    Se agregan títulos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Problema uno"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
